f6347edbca4451e94c89f9083bd4e0c04508dc6d
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-03-13 14:59:25 -0400

    Set to 1ms timer

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -88,7 +88,7 @@ void MX_TIM17_Init(void)
   htim17.Instance = TIM17;
   htim17.Init.Prescaler = 15999;
   htim17.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim17.Init.Period = 99;
+  htim17.Init.Period = 9;
   htim17.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim17.Init.RepetitionCounter = 0;
   htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;

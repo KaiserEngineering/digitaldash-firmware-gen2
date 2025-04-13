9dc303dd0d2603b9feb8fcfd3c5a3cbc50ed76a4
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-04-12 22:21:02 -0400

    Add EEPROM read/write

--- scripts/generate-settings.py
+++ scripts/generate-settings.py
@@ -576,6 +576,11 @@ for i, struct in enumerate(config["config"]["struct_list"]):
             write_define_load_setting( config_c, struct, cmd, 1)
 config_c.write("\n")
 
+config_c.write("static settings_write *write;\n")
+config_c.write("static settings_read *read;\n\n")
+config_c.write("void settings_setWriteHandler(settings_write *writeHandler) { write = writeHandler; }\n")
+config_c.write("void settings_setReadHandler(settings_read *readHandler) { read = readHandler; }\n\n")
+
 config_h.write("\n\nvoid load_settings(void);\n")
 config_c.write("void load_settings(void)\n{\n")
 for i, struct in enumerate(config["config"]["struct_list"]):

e7d35b41a1aa8daca726835707a42bb019541975
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-04-30 19:59:11 -0400

    Declare themes

--- ui/themes.h
+++ ui/themes.h
@@ -10,6 +10,11 @@
 
 #include "ui.h"
 
+lv_obj_t * add_grumpy_cat_gauge( int32_t x, int32_t y, lv_obj_t * parent, PID_DATA * pid);
+lv_obj_t * add_linear_gauge( int32_t x, int32_t y, lv_obj_t * parent, PID_DATA * pid);
+lv_obj_t * add_radial_gauge( int32_t x, int32_t y, lv_obj_t * parent, PID_DATA * pid);
+lv_obj_t * add_stock_st_gauge( int32_t x, int32_t y, lv_obj_t * parent, PID_DATA * pid);
+
 lv_obj_t * add_gauge( GAUGE_THEME theme, int32_t x, int32_t y, lv_obj_t * parent, PID_DATA * pid);
 
 #endif /* THEMES_H_ */

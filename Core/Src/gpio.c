ca86812dce161472f5d8e4dd0e8e93295c9d8023
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-08-29 20:11:21 -0400

    Fix ESP32 RESET default state

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -59,10 +59,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOI_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(HSPI_NRST_GPIO_Port, HSPI_NRST_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(ESP32_RESET_N_GPIO_Port, ESP32_RESET_N_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, HSPI_NRST_Pin|ESP32_RESET_N_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LCD_EN_GPIO_Port, LCD_EN_Pin, GPIO_PIN_RESET);
